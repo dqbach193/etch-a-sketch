a72e86a07eb310ba4315ded2bef7ec2199c294c4
yeutv <dqbach193@gmail.com>
2023-07-03 20:12:22 +0700

    remove break divs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-​
+
 function createDivs() {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
@@ -8,10 +8,7 @@ function createDivs() {
             newDiv.classList.add("square");
             container.appendChild(newDiv);
         }
-        const breakDiv = document.createElement("div");
-        breakDiv.classList.add("break");
-        container.appendChild(breakDiv);
     }
 }
-​
+
 createDivs();
