b081a49b83cc7218dc6b9c5846031656bdf5b534
yeutv <dqbach193@gmail.com>
2024-03-29 19:15:27 +0700

    add making squares button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container = document.getElementById('container');
 
-function createDivs() {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+
+function createDivs(num) {
+    for (let i = 0; i < num; i++) {
+        const breakLine = document.createElement("div");
+        breakLine.classList.add('break-line');
+        container.appendChild(breakLine);
+        for (let j = 0; j < num; j++) {
             const newDiv = document.createElement("div");
             newDiv.innerText= j+1;
             newDiv.classList.add("square");
@@ -11,14 +15,35 @@ function createDivs() {
     }
 }
 
-createDivs();
+createDivs(16);
 
 const squares = document.getElementsByClassName('square');
 
-console.log(squares);
-
-for(let i = 0; i < squares.length; i++){
-    squares[i].addEventListener('mouseover', ()=>{
-        squares[i].classList.add('square-hover');
-    });
+function addHoverClass(){
+    for(let i = 0; i < squares.length; i++){
+        squares[i].addEventListener('mouseover', ()=>{
+            squares[i].classList.add('square-hover');
+        });
+    }
 }
+addHoverClass();
+
+
+const squareInit = document.getElementById("square-init");
+
+squareInit.addEventListener('click',()=>{
+    let num = prompt("Enter the number of squares");
+    
+    if(num == Math.sqrt(squares.length)){
+        return;
+    }else if(num > 100){
+        alert('Please enter a number lower than 100');
+    }else{
+        container.replaceChildren();
+        createDivs(num);
+        addHoverClass();
+    }
+})
+
+
+
